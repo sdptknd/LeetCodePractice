fb30576d6c974da861aa4d58750f2083ef4c1bd3
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-02-17 08:59:23 +0530

    Time: 2 ms (27.99%), Space: 28.7 MB (64.05%)

--- 0062-unique-paths/0062-unique-paths-Solution2.cs
+++ 0062-unique-paths/0062-unique-paths-Solution2.cs
@@ -1,38 +1,27 @@
 public class Solution {
     public int UniquePaths(int m, int n) {
-        if(m == 1 || n == 1) return 1;
-        var matrix = new int[m, n];
-        matrix[m-1, n-1] = 1;
-        int row = m - 2, col = n - 2;
+        var paths = new int[m,n];
+        // Console.WriteLine($"RowLen: {paths.GetLength(0)}, colLen: {paths.GetLength(1)}");
+        return FindPathsCount(0, 0, paths);
+    }
 
-        while(row >= 0 && col >= 0){
-            Console.WriteLine($"row: {row}, col: {col}");
-            for(int i = n-1; i > col; i--){
-                if(matrix[row,i] > 0) continue;
-                var paths = 0;
-                //adding right path
-                paths += i == n-1 ? 0 : matrix[row, i+1];
-                //adding down path
-                paths += matrix[row+1, i];
-                Console.WriteLine($"Adding Rowwise: i: {i}");
-                matrix[row,i] = paths;
-            }
-            for(int j = m-1; j > row; j--){
-                if(matrix[j,col] > 0) continue;
-                var paths = 0;
-                //adding right path
-                paths += matrix[j, col+1];
-                //adding down path
-                paths += j == m-1 ? 0 : matrix[j+1, col];
-                Console.WriteLine($"Adding Columnwise: j: {j}");
-                matrix[j,col] = paths;
-            }
-            matrix[row,col] = matrix[row,col+1] + matrix[row+1,col];
-            if(row ==0 && col ==0) break;
-            row = row == 0 ? 0 : row - 1;
-            col = col == 0 ? 0 : col - 1;
+    public int FindPathsCount(int row, int col, int[,] paths){
+        // Console.WriteLine($"Checking for row: {row} and col: {col}");
+        if(paths[row, col] > 0) return paths[row, col];
+        if(row == paths.GetLength(0) - 1 && col == paths.GetLength(1) - 1){
+            paths[row, col] = 1;
+            return 1;
         }
 
-        return matrix[0,0];
+        var pathsCount = 0;
+
+        // Console.WriteLine($"Calculate from adjacent");
+
+        if(row < paths.GetLength(0) - 1) pathsCount += FindPathsCount(row+1, col, paths);
+
+        if(col < paths.GetLength(1) - 1) pathsCount += FindPathsCount(row, col+1, paths);
+
+        paths[row, col] = pathsCount;
+        return pathsCount;
     }
 }
