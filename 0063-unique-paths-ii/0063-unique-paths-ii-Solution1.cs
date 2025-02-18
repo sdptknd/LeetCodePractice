c47e90edb96bb1c1569a5e25d803000e2c571551
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-02-18 08:12:38 +0530

    Time: 0 ms (100%), Space: 41.3 MB (69.69%)

--- 0063-unique-paths-ii/0063-unique-paths-ii-Solution1.cs
+++ 0063-unique-paths-ii/0063-unique-paths-ii-Solution1.cs
@@ -2,19 +2,20 @@ public class Solution {
     public int UniquePathsWithObstacles(int[][] obstacleGrid) {
         var m = obstacleGrid.Length;
         var n = obstacleGrid[0].Length;
+        // if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
         int[,] matrix = new int[m,n];
-        matrix[m-1, n-1] = 1- obstacleGrid[m-1][n-1];
+        matrix[0,0] = 1 - obstacleGrid[0][0];
 
-        for(int i = m-2; i>=0; i--)
-            matrix[i,n-1] = obstacleGrid[i][n-1] == 1 ? 0 : matrix[i+1,n-1];
+        for(int i = 1; i < m; i++)
+            matrix[i,0] = obstacleGrid[i][0] == 1 ? 0 : matrix[i-1,0];
         
-        for(int i = n-2; i>=0; i--)
-            matrix[m-1,i] = obstacleGrid[m-1][i] == 1 ? 0 : matrix[m-1,i+1];
+        for(int i = 1; i < n; i++)
+            matrix[0,i] = obstacleGrid[0][i] == 1 ? 0 : matrix[0,i-1];
 
-        for(int row = m-2; row>=0; row--)
-            for(int col = n-2; col>=0; col--)
-                matrix[row,col] = obstacleGrid[row][col] == 1 ? 0 : (matrix[row+1,col] + matrix[row,col+1]);
+        for(int row = 1; row<m; row++)
+            for(int col = 1; col<n; col++)
+                matrix[row,col] = obstacleGrid[row][col] == 1 ? 0 : (matrix[row-1,col] + matrix[row,col-1]);
         
-        return matrix[0,0];
+        return matrix[m-1,n-1];
     }
 }
