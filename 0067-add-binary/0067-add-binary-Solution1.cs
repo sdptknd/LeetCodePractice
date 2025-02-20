fa35bd09a99b7492efaf73e4f79d982b1c377349
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-02-20 08:09:49 +0530

    Time: 2 ms (63.39%), Space: 41.6 MB (66.71%)

--- 0067-add-binary/0067-add-binary-Solution1.cs
+++ 0067-add-binary/0067-add-binary-Solution1.cs
@@ -1,27 +1,18 @@
 public class Solution {
     public string AddBinary(string a, string b) {
         var result = new StringBuilder();
-        var smallNum = a.Length > b.Length ? b : a;
-        var bigNum = a.Length > b.Length ? a : b;
-        var sPos = smallNum.Length - 1;
-        var bPos = bigNum.Length - 1;
+        var aPos = a.Length - 1;
+        var bPos = b.Length - 1;
         var carry = 0;
 
-        while(sPos >= 0){
-            var posVal = int.Parse(smallNum[sPos--].ToString()) + int.Parse(bigNum[bPos--].ToString()) + carry;
+        while(aPos >= 0 || bPos >= 0 || carry > 0){
+            var term1 = aPos >= 0 ? a[aPos--] - '0' : 0;
+            var term2 = bPos >= 0 ? b[bPos--] - '0' : 0;
+            var posVal = term1 + term2 + carry;
             var posDigit = posVal % 2;
             carry = posVal / 2;
             result.Insert(0, posDigit);
         }
-
-        while(bPos >= 0){
-            var posVal = int.Parse(bigNum[bPos--].ToString()) + carry;
-            var posDigit = posVal % 2;
-            carry = posVal / 2;
-            result.Insert(0, posDigit);
-        }
-
-        if(carry > 0) result.Insert(0, carry);
 
         return result.ToString();
     }
