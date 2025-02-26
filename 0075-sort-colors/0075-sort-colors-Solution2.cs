169003b7c7a03327937065a611ae85958854232c
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-02-26 08:04:21 +0530

    Time: 0 ms (100%), Space: 46 MB (79.29%)

--- 0075-sort-colors/0075-sort-colors-Solution2.cs
+++ 0075-sort-colors/0075-sort-colors-Solution2.cs
@@ -1,15 +1,17 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int i = 0, j = nums.Length - 1;
+        var sortedNextIndx = ProcessColour(nums, 0, 0);
+        ProcessColour(nums, 1, sortedNextIndx);
+    }
 
+    private int ProcessColour(int[] nums, int colour, int start){
+        int i = start, j = nums.Length - 1;
         while(i < j){
-            // Console.WriteLine($"i: {i}, j: {j}, nums: {string.Join(',', nums)}");
-            if(nums[i] == 0){
+            if(nums[i] == colour){
                 i++;
                 continue;
             }
-            // Console.WriteLine($"i: {i}");
-            if(i < j && nums[j] == 0) {
+            if(i < j && nums[j] == colour) {
                 var temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
@@ -17,21 +19,8 @@ public class Solution {
             j--;
         }
 
-        j = nums.Length - 1;
-        while(i < nums.Length && nums[i] == 0) i++;
+        while(i < nums.Length && nums[i] == colour) i++;
 
-        while(i < j){
-            // Console.WriteLine($"i: {i}, j: {j}, nums: {string.Join(',', nums)}");
-            if(nums[i] == 1){
-                i++;
-                continue;
-            }
-            if(i < j && nums[j] == 1) {
-                var temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
-            j--;
-        }
+        return i;
     }
 }
