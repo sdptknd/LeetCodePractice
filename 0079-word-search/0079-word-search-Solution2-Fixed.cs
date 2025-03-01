aa0dae411f027867c81140273a84b25374cd5384
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-01 09:13:05 +0530

    Time: 142 ms (92.5%), Space: 43.5 MB (56.87%)

--- 0079-word-search/0079-word-search-Solution2-Fixed.cs
+++ 0079-word-search/0079-word-search-Solution2-Fixed.cs
@@ -1,22 +1,4 @@
 public class Solution {
-    private IEnumerable<(int, int)> PossibleNextCells(int row, int col, char[][] board){
-        var nextCell = (row + 1, col);
-        if(nextCell.Item1 < board.Length)
-            yield return nextCell;
-
-        nextCell = (row - 1, col);
-        if(nextCell.Item1 >= 0)
-            yield return nextCell;
-        
-        nextCell = (row, col+1);
-        if(nextCell.Item2 < board[0].Length)
-            yield return nextCell;
-        
-        nextCell = (row, col-1);
-        if(nextCell.Item2 >= 0)
-            yield return nextCell;
-    }
-
     public bool Exist(char[][] board, string word) {
         for(int i = 0; i<board.Length; i++){
             for(int j = 0; j<board[0].Length; j++){
@@ -32,10 +14,6 @@ public class Solution {
 
         var tmp = board[row][col];
         board[row][col] = '\0';
-
-        // var validPath = PossibleNextCells(row, col, board)
-        //         .Any(nextCell => 
-        //             Explore(board, nextCell.Item1, nextCell.Item2, charIndx+1, word));
 
         var validPath = (row + 1 < board.Length && Explore(board, row + 1, col, charIndx+1, word))
             || (row > 0 && Explore(board, row - 1, col, charIndx+1, word))
