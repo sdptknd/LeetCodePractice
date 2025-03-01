12fc70de8e84da9cdcb2a4b50a58a2d343dc5064
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-01 09:13:38 +0530

    Time: 136 ms (92.86%), Space: 43.7 MB (31.61%)

--- 0079-word-search/0079-word-search-Solution2.cs
+++ 0079-word-search/0079-word-search-Solution2.cs
@@ -1,35 +1,27 @@
 public class Solution {
     public bool Exist(char[][] board, string word) {
-        int m = board.Length;
-        int n = board[0].Length;
-        
-        Func<int, int, int, bool> backtrack = null;
-        backtrack = (i, j, k) => {
-            if (k == word.Length) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            bool result = backtrack(i + 1, j, k + 1) || 
-                          backtrack(i - 1, j, k + 1) || 
-                          backtrack(i, j + 1, k + 1) || 
-                          backtrack(i, j - 1, k + 1);
-            
-            board[i][j] = temp; 
-            return result;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
-                }
+        for(int i = 0; i<board.Length; i++){
+            for(int j = 0; j<board[0].Length; j++){
+                if(Explore(board, i, j, 0, word)) return true;
             }
         }
         return false;
+    }
+
+    public bool Explore(char[][] board, int row, int col, int charIndx, string word){
+        if(board[row][col] != word[charIndx]) return false;
+        if(charIndx == word.Length -1) return true;
+
+        var tmp = board[row][col];
+        board[row][col] = '\0';
+
+        var validPath = (row + 1 < board.Length && Explore(board, row + 1, col, charIndx+1, word))
+            || (row > 0 && Explore(board, row - 1, col, charIndx+1, word))
+            || (col + 1 < board[0].Length && Explore(board, row, col + 1, charIndx+1, word))
+            || (col > 0 && Explore(board, row, col - 1, charIndx+1, word));
+        
+        board[row][col] = tmp;
+
+        return validPath;
     }
 }
