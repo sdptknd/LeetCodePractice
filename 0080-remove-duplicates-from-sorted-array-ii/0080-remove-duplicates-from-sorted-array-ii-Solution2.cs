7de2f491b4d219a92db31baf428d8e48e2b7c110
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-02 07:43:09 +0530

    Time: 141 ms (35.51%), Space: 52.4 MB (41.11%)

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-Solution2.cs
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-Solution2.cs
@@ -3,10 +3,14 @@ public class Solution {
         if(nums.Length <= 2) return nums.Length;
 
         var shiftedIndx = 2;
+        var lastNum1 = nums[0];
+        var lastNum2 = nums[1];
 
         for(int i = 2; i < nums.Length; i++){
-            if(nums[i] == nums[shiftedIndx-1] && nums[i] == nums[shiftedIndx-2]) continue;
+            if(nums[i] == lastNum1 && nums[i] == lastNum2) continue;
             nums[shiftedIndx++] = nums[i];
+            lastNum1 = lastNum2;
+            lastNum2 = nums[i];
         }
 
         return shiftedIndx;
