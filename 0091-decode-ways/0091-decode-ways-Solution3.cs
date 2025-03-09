f659b1349a62b4df2a6e0283ca40046aba42809a
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-09 08:36:12 +0530

    Time: 3 ms (26.04%), Space: 40.1 MB (12.04%)

--- 0091-decode-ways/0091-decode-ways-Solution3.cs
+++ 0091-decode-ways/0091-decode-ways-Solution3.cs
@@ -1,29 +1,28 @@
 public class Solution {
     public int NumDecodings(string s) {
-        var digits = s.ToArray().Select(c => int.Parse(c.ToString())).ToList();
-        return NumDecodings2(digits, 0, new Dictionary<int,int>());
+        return NumDecodings2(s, 0, new Dictionary<int,int>(){{s.Length, 1}});
     }
 
-    public int NumDecodings2(List<int> d, int indx, Dictionary<int,int> memo){
-        if(indx >= d.Count) return 1;
+    public int NumDecodings2(string s, int indx, Dictionary<int,int> memo){
+        // if(indx >= s.Length) return 1;
         if(memo.TryGetValue(indx, out var numDecodings)) return numDecodings;
 
         var currPosNumDecodings = 0;
-        var currDigit = d[indx];
+        var currDigit = int.Parse(s[indx].ToString());
 
         if(currDigit > 0 && currDigit <= 26)
-            currPosNumDecodings += NumDecodings2(d, indx+1, memo);
+            currPosNumDecodings += NumDecodings2(s, indx+1, memo);
         
-        if(indx == d.Count - 1) {
+        if(indx == s.Length - 1) {
             memo[indx] = currPosNumDecodings;
             return currPosNumDecodings;
         }
 
-        var nextDigit = d[indx+1];
-        var num = currDigit * 10 + nextDigit;
+        var nextDigit = int.Parse(s[indx+1].ToString());
+        // var num = currDigit * 10 + nextDigit;
 
-        if(num > 9 && num <= 26)
-            currPosNumDecodings += NumDecodings2(d, indx+2, memo);
+        if(currDigit == 1 || (currDigit == 2 && nextDigit < 7))
+            currPosNumDecodings += NumDecodings2(s, indx+2, memo);
 
         memo[indx] = currPosNumDecodings;
         return currPosNumDecodings;
