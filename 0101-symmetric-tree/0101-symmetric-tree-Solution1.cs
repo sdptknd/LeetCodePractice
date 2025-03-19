4f536ccdbdabf97d739cb1d5acec380cb91e19f5
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-19 08:11:49 +0530

    Time: 0 ms (100%), Space: 44 MB (93.3%)

---
+++ 0101-symmetric-tree/0101-symmetric-tree-Solution1.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSymmetric(TreeNode root) {
+        return AreSymmetricEqual(root.left, root.right);
+    }
+
+    public bool AreSymmetricEqual(TreeNode node1, TreeNode node2) {
+        if(node1 == null && node2 == null) return true;
+        if(node1 == null || node2 == null) return false;
+        if(node1.val != node2.val) return false;
+
+        return AreSymmetricEqual(node1.left, node2.right) &&
+               AreSymmetricEqual(node1.right, node2.left);
+    }
+}
