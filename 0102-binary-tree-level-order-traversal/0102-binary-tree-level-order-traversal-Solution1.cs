b6ac7ddd8772c61e616b022e6c893d77a1a90edd
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-03-20 07:33:20 +0530

    Time: 7 ms (3.91%), Space: 50 MB (6.26%)

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal-Solution1.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        var result = new List<IList<int>> ();
+        if(root == null) return result;
+        var currLevel = new List<TreeNode> { root };
+
+        while(currLevel.Any()){
+            // Console.WriteLine(string.Join(',', currLevel.Select(node => node.val)));
+            result.AddRange(currLevel.Select(node => node.val).ToList());
+            currLevel = 
+                currLevel
+                .SelectMany(node => new List<TreeNode> {node.left, node.right})
+                .Where(node => node != null).ToList();
+        }
+
+        return result;
+    }
+}
