dfe1ae4263b68f612abd6a20c31208582dd3ef6e
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2025-04-11 07:38:44 +0530

    Time: 0 ms (100%), Space: 43.7 MB (29.87%)

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii-Solution1.cs
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii-Solution1.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
         var profit = 0;
-        var buy = prices[0];
 
         for(var i = 1; i < prices.Length; i++){
-            if(prices[i] > buy) profit += (prices[i] - buy);
-            buy = prices[i];
+            if(prices[i] > prices[i-1]) profit += (prices[i] - prices[i-1]);
         }
 
         return profit;
