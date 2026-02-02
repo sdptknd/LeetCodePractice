e2e827c41364618e1eab799eea66a85f8b0e5d39
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2026-02-02 10:55:15 +0530

    Time: 66 ms (93.58%), Space: 42.8 MB (81.28%)

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer-Solution3.cs
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer-Solution3.cs
@@ -21,10 +21,11 @@ public class Solution {
 
     public Node CopyListOnly(Node node, Dictionary<Node, Node> nodesMapping) {
         if(node == null) return null;
+        if(nodesMapping.ContainsKey(node)) return nodesMapping[node];
         var copiedNode = new Node(node.val);
         nodesMapping.Add(node, copiedNode);
-        copiedNode.next = node.next == null ? null : (nodesMapping.TryGetValue(node.next, out var nextNode) ? nextNode : CopyListOnly(node.next, nodesMapping));
-        copiedNode.random = node.random == null ? null : (nodesMapping.TryGetValue(node.random, out var randomNode) ? randomNode : CopyListOnly(node.random, nodesMapping));
+        copiedNode.next = CopyListOnly(node.next, nodesMapping);
+        copiedNode.random = CopyListOnly(node.random, nodesMapping);
         return copiedNode;
     }
 
