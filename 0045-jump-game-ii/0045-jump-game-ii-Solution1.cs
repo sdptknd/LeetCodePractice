216f59972c690a7da3687bdb6a294454fe643464
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2026-02-14 22:08:59 +0530

    Time: 188 ms (10.41%), Space: 50.4 MB (5.2%)

--- 0045-jump-game-ii/0045-jump-game-ii-Solution1.cs
+++ 0045-jump-game-ii/0045-jump-game-ii-Solution1.cs
@@ -1,18 +1,37 @@
 public class Solution {
     public int Jump(int[] nums) {
-        return JumpCount(nums, 0);
+        var memo = new Dictionary<int, int>{{nums.Length - 1, 0}};
+
+        for(var i = nums.Length - 2; i >= 0; i--){
+            int minJumpCount = int.MaxValue;
+            for(int step = 1; step <= nums[i]; step++){
+                if(i + step < nums.Length){
+                    var nextMinJumpCount = memo[i + step];
+                    if(nextMinJumpCount < minJumpCount) minJumpCount = nextMinJumpCount;
+                }
+            }
+            memo[i] = minJumpCount == int.MaxValue ? int.MaxValue : minJumpCount + 1;
+        }
+
+        return memo[0];
     }
 
-    public int JumpCount(int[] nums, int index){
-        if(index == nums.Length - 1) return 0;
+    private int MinJump(int[] nums, int leftIndex, Dictionary<int, int> memo){
+        if(leftIndex == nums.Length - 1) return 0;
 
-        if(nums[index] >= nums.Length - 1 - index) return 1;
+        if(memo.TryGetValue(leftIndex, out var ans)) return ans;
 
-        var max = index + 1;
-        for(int i = index + 1; i <= index + nums[index]; i++){
-            if(nums[i] == 0) continue;
-            if(nums[i] + i >= nums[max] + max) max = i;
+        int minJumpCount = int.MaxValue;
+
+        for(int step = 1; step <= nums[leftIndex]; step++){
+            if(leftIndex + step < nums.Length){
+                var nextMinJumpCount = MinJump(nums, leftIndex + step, memo);
+                if(nextMinJumpCount < minJumpCount) minJumpCount = nextMinJumpCount;
+            }
         }
-        return 1 + JumpCount(nums, max);
+
+        ans = minJumpCount == int.MaxValue ? int.MaxValue : minJumpCount + 1;
+        memo[leftIndex] = ans;
+        return ans;
     }
 }
