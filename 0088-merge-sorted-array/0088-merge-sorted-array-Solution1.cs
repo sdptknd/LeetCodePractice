3be84ad8de4d93ce4cf0328559344c682609de59
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2026-02-15 18:08:15 +0530

    Time: 0 ms (100%), Space: 47.2 MB (60.13%)

--- 0088-merge-sorted-array/0088-merge-sorted-array-Solution1.cs
+++ 0088-merge-sorted-array/0088-merge-sorted-array-Solution1.cs
@@ -1,12 +1,17 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1, j = nums1.Length -1, k = n-1;
+        int index1 = m - 1, index2 = n - 1;
 
-        while(i >= 0 && k >= 0){
-            if(nums1[i] > nums2[k]) nums1[j--] = nums1[i--];
-            else nums1[j--] = nums2[k--];
+        for(int i = m + n - 1; i >= 0; i--){
+            if(index1 < 0){
+                nums1[i] = nums2[index2--];
+            } else if(index2 < 0){
+                nums1[i] = nums1[index1--];
+            } else if(nums1[index1] > nums2[index2]){
+                nums1[i] = nums1[index1--];
+            } else {
+                nums1[i] = nums2[index2--];
+            }
         }
-
-        while(k >= 0) nums1[j--] = nums2[k--];
     }
 }
