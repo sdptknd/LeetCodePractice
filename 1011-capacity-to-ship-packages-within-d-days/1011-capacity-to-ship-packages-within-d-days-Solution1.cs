11143195e69f6a98ea1f1185594a21c512ddb533
Sudipta Kundu <58025792+sdptknd@users.noreply.github.com>
2026-02-17 22:20:10 +0530

    Time: 16 ms (58.8%), Space: 53.5 MB (58.8%)

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days-Solution1.cs
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days-Solution1.cs
@@ -1,38 +1,37 @@
 public class Solution {
     public int ShipWithinDays(int[] weights, int days) {
-        int minCapacity = 0;
-        int maxCapacity = 0;
+        int max = 1, sum = 0;
 
         foreach(var weight in weights){
-            minCapacity = Math.Max(minCapacity, weight);
-            maxCapacity += weight;
+            max = Math.Max(weight, max);
+            sum += weight;
         }
 
-        // Console.WriteLine(minCapacity);
-        // Console.WriteLine(maxCapacity);
+        int left = max, right = sum;
 
-        while(minCapacity <= maxCapacity){
-            int mid = minCapacity + (maxCapacity - minCapacity) / 2;
-            var canShip = CanShip(weights, days, mid);
-            // Console.WriteLine($"min: {minCapacity}, max: {maxCapacity}, mid: {mid}, canShip: {canShip}");
-            if(canShip) maxCapacity = mid - 1;
-            else minCapacity = mid + 1;
+        while(left <= right){
+            var middle = left + ((right - left) / 2);
+            if(PossibleToShip(weights, middle, days)) right = middle - 1;
+            else left = middle + 1;
         }
 
-        return minCapacity;
+        return left;
     }
 
-    private bool CanShip(int[] weights, int days, int capacity){
-        var singleDayShipLoad = 0;
+    private bool PossibleToShip(int[] weights, int capacity, int days){
+        int currLoad = 0, requiredDays = 0;
+
         foreach(var weight in weights){
-            if(capacity - singleDayShipLoad < weight){
-                days--;
-                if(days == 0) return false;
-                singleDayShipLoad = 0;
+            currLoad += weight;
+            if(currLoad > capacity){
+                requiredDays++;
+                currLoad = weight;
             }
-            singleDayShipLoad += weight;
         }
 
-        return true;
+        if(currLoad > 0) requiredDays++;
+
+        Console.WriteLine($"capacity: {capacity}, requiredDays: {requiredDays}");
+        return requiredDays <= days;
     }
 }
